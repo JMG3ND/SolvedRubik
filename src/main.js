040ce817835073ddf891621c7cd9cad120e6feac
José Gutiérrez <jo0303se@hotmail.com>
2023-04-13 11:46:52 -0500

    commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,41 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+
 import App from './App.vue'
 import router from './router'
 
+/* add icons to the library */
+library.add(faMoon)
+library.add(faSun)
+library.add(faMagnifyingGlass)
+library.add(faBars)
+library.add(faCaretDown)
+library.add(faFacebook)
+library.add(faTwitter)
+library.add(faInstagram)
+library.add(faYoutube)
+library.add(faWhatsapp)
+
 const app = createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
