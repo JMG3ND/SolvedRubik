8c9a9827ddccfe71e8806efa61e11f80d1749a82
José Gutiérrez <jo0303se@hotmail.com>
2023-04-27 19:06:19 -0500

    commit

--- src/views/articlesCube3x3/beginnerview/BottomCornersView.vue
+++ src/views/articlesCube3x3/beginnerview/BottomCornersView.vue
@@ -3,5 +3,6 @@
 <template>
     <main class="main-article">
         <h1>Esquinas Inferiores</h1>
+        <p>En este paso se deben introducir las esquinas inferiores del cubo en su ubicación correspondiente</p>
     </main>
 </template>
