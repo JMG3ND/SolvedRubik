435db0f5842516d47226a3c53acd44b653c1f130
José Gutiérrez <jo0303se@hotmail.com>
2023-04-28 20:10:33 -0500

    commit

---
+++ src/components/ArticleItem.vue
@@ -0,0 +1,92 @@
+<template>
+    <section class="article-item">
+        <div class="article-item__image-container"><img class="article-item__image" :src="imageArticle"
+                alt="Cubo de rubik 2x2"></div>
+        <div class="article-item__informataion-container">
+            <header class="article-item__title-container">
+                <h2 class="article-item__title">{{ title }}</h2>
+            </header>
+            <article class="article-item__description-container">
+                <p class="article-item__description">{{ description }}</p>
+            </article>
+            <footer class="article-item__link-container">
+                <RouterLink to="/cube-three-by-three" class="button-link">Ir</RouterLink>
+            </footer>
+        </div>
+    </section>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+
+defineProps({
+    title: String,
+    imageArticle: URL,
+    description: String
+});
+</script>
+
+<style lang="scss">
+@import '@/assets/_colors-theme.scss';
+
+.article-item {
+    position: relative;
+    grid-template-columns: 0.8fr 1fr;
+    display: grid;
+    border-radius: 20px;
+    padding: 0.5rem;
+    background-color: $dark-baground-color-z-index-1;
+
+    body.light & {
+        background-color: $light-baground-color-z-index-1;
+    }
+
+    &__informataion-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    &__image-container {
+        display: flex;
+        align-items: center;
+        margin-right: 0.5rem;
+    }
+
+    &__image {
+        width: 100%;
+        padding: 10px;
+        background-color: $dark-baground-color-z-index-2;
+
+        body.light & {
+            background-color: $light-baground-color-z-index-3;
+        }
+
+        border-radius: 10px;
+    }
+
+    &__title {
+        margin-bottom: 0.5rem;
+    }
+
+    &__description-container {
+        height: 100%;
+    }
+
+    &__link-container {
+        text-align: end;
+    }
+}
+
+.button-link {
+    display: inline-block;
+    text-decoration: none;
+    padding: 10px 40px 10px 40px;
+    border-radius: 10px;
+    background-color: $dark-baground-color-z-index-3;
+
+    body.light & {
+        background-color: $light-baground-color-z-index-3;
+    }
+}
+</style>
--- src/components/HomPageInformation.vue
+++ src/components/HomPageInformation.vue
@@ -5,24 +5,9 @@
         <h1 class="home-page-information__title">ARTÍCULOS</h1>
       </header>
       <article class="home-page-information__item-container">
-
         <!--Block-->
-        <section class="article-item">
-          <div class="article-item__image-container"><img class="article-item__image" :src="cube3x3Img"
-              alt="Cubo de rubik 2x2"></div>
-          <div class="article-item__informataion-container">
-            <header class="article-item__title-container">
-              <h2 class="article-item__title">CUBO 3X3</h2>
-            </header>
-            <article class="article-item__description-container">
-              <p class="article-item__description">Algoritmos para resolver el cubo de rubik tres por tres</p>
-            </article>
-            <footer class="article-item__link-container">
-              <RouterLink to="/cube-three-by-three" class="button-link">Ir</RouterLink>
-            </footer>
-          </div>
-        </section>
-
+        <ArticleItem title="CUBO 3X3" :imageArticle="cube3x3"
+          description="Algoritmos para resolver el cubo tres por tres" />
       </article>
     </div>
   </section>
@@ -33,16 +18,24 @@
         <h1 class="home-page-information__title">ARTÍCULOS EN DESARROLLO</h1>
       </header>
       <article class="home-page-information__item-container">
-
-
+        <!--Block-->
+        <ArticleItem title="CUBO 2X2" :imageArticle="cube2x2"
+          description="Algoritmos para resolver el cubo dos por dos" />
+        <ArticleItem title="CUBO 4X4" :imageArticle="cube4x4"
+          description="Algoritmos para resolver el cubo cuatro por cuatro" />
+        <ArticleItem title="CUBO 2X20" :imageArticle="cube5x5"
+          description="Algoritmos para resolver el cubo cinco por cinco" />
       </article>
     </div>
   </section>
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router';
-import cube3x3Img from '../assets/images/home-page-article/cubo-tres-por-tres.png'
+import ArticleItem from './ArticleItem.vue';
+import cube2x2 from '../assets/images/home-page-article/cubo-dos-por-dos.png';
+import cube3x3 from '../assets/images/home-page-article/cubo-tres-por-tres.png';
+import cube4x4 from '../assets/images/home-page-article/cubo-cuatro-por-cuatro.png';
+import cube5x5 from '../assets/images/home-page-article/cubo-cinco-por-cinco.png';
 </script>
 
 <style lang="scss">
@@ -71,67 +64,6 @@ import cube3x3Img from '../assets/images/home-page-article/cubo-tres-por-tres.pn
     &__item-container {
       grid-template-columns: 1fr;
     }
-  }
-}
-
-.article-item {
-  position: relative;
-  grid-template-columns: 0.8fr 1fr;
-  display: grid;
-  border-radius: 20px;
-  padding: 0.5rem;
-  background-color: $dark-baground-color-z-index-1;
-
-  body.light & {
-    background-color: $light-baground-color-z-index-1;
-  }
-
-  &__informataion-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-
-  &__image-container {
-    display: flex;
-    align-items: center;
-    margin-right: 0.5rem;
-  }
-
-  &__image {
-    width: 100%;
-    padding: 10px;
-    background-color: $dark-baground-color-z-index-2;
-
-    body.light & {
-      background-color: $light-baground-color-z-index-3;
-    }
-
-    border-radius: 10px;
-  }
-
-  &__title {
-    margin-bottom: 0.5rem;
-  }
-
-  &__description-container {
-    height: 100%;
-  }
-
-  &__link-container {
-    text-align: end;
-  }
-}
-
-.button-link {
-  display: inline-block;
-  text-decoration: none;
-  padding: 10px 40px 10px 40px;
-  border-radius: 10px;
-  background-color: $dark-baground-color-z-index-3;
-
-  body.light & {
-    background-color: $light-baground-color-z-index-3;
   }
 }
 </style>
