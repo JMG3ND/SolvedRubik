84351ce976c30195676822e87dc649fbe20ab655
José Gutiérrez <jo0303se@hotmail.com>
2023-05-01 23:54:17 -0500

    commit

--- src/views/articlesCube3x3/fridrichview/OLLView.vue
+++ src/views/articlesCube3x3/fridrichview/OLLView.vue
@@ -1,5 +1,15 @@
-<script setup></script>
+<script setup>
+import CardAlgorithm from '@/components/CardAlgorithm.vue';
+</script>
 
 <template>
-    <h1>OLL</h1>
+    <section class="main-section">
+        <header class="main-section__header">
+            <h1 class="main-section__title">OLL</h1>
+        </header>
+        <article class="main-section__article">
+            <CardAlgorithm description="R' U' R U2 R' U' R2 B R' U' R' U R B' R' U R U R' U R" />
+        </article>
+    </section>
 </template>
+
