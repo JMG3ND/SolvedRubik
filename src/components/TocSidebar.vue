81fe2874a1220771fd6f8b23d6ad027d2eac1077
José Gutiérrez <jo0303se@hotmail.com>
2023-05-08 13:01:48 -0500

    commit

--- src/components/TocSidebar.vue
+++ src/components/TocSidebar.vue
@@ -1,9 +1,11 @@
 <template>
+    <!--Menú de navegación que se utiliza para navegar entre las distíntas secciones del contenido main-->
     <aside ref="asidevar" class="toc-sidebar">
         <h2 class="toc-sidebar__title">En este artículo</h2>
         <nav class="toc-sidebar__nav">
             <ul class="toc-sidebar__list">
                 <li class="toc-sidebar__item" v-for="element in  data ">
+                    <!--Consiste en la utilización de un ancla dinámico que se obtiene del arreglo con la propiedad id-->
                     <a class="toc-sidebar__link" :href="`#${element.id}`">
                         {{ element.title }}
                     </a>
@@ -14,9 +16,22 @@
 </template>
 
 <script setup>
+/*
+Recibe una data que debe ser un arreglo de objetos
+Con el siguiente formato
+
+Array = [{title: String, id: String}];
+
+Si se pasa un arreglo que no contenga estas dos propiedades el componente no funciona;
+*/
 defineProps({
     data: Array
 });
+
+/* 
+Se quiere que las anclas se resalten cuando el usuario se encuentra en una sección en específico de la página.
+
+*/
 </script>
 
 <style lang="scss">
